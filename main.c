cffe9a5033a4e65950640060dbe6a2feb6a3ebe1
Ben <benmchoo@gmail.com>
2020-04-05 16:14:58 -0400

    setup makefile and tasks.json

--- main.c
+++ main.c
@@ -1,4 +1,14 @@
 #include "mylib.h"
 
+//Prototypes
+
+//Variables
+unsigned short buttons;
+unsigned short oldButtons;
+
 int main() {
+    while(1) {
+        oldButtons = buttons;
+        buttons = BUTTONS;
+    }
 }
